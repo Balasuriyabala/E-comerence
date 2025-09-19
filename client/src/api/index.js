9a10eeef9044abf0fd7cdbbe0194972e0b82e1c9
Balasuriya <111849019+Balasuriyabala@users.noreply.github.com>
2025-09-19 15:04:41 +0530

    Update index.js
    removed / before 8001

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({ 
-  baseURL: "http://192.168.29.46/:8001/api/",
+  baseURL: "http://192.168.29.46:8001/api/",
   //baseURL: process.env.REACT_APP_API_URL,
   //baseURL: "http://localhost:8001/api/",
 });
