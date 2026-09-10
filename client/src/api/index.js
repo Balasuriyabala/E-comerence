b87137341ecd82cb9fe3847202ec5ef9da6e8fd3
Balasuriya <111849019+Balasuriyabala@users.noreply.github.com>
2026-09-10 14:39:33 +0530

    Update API base URL to new port

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({ 
   //baseURL: import.meta.env.VITE_API_URL,
-  baseURL: "http://192.168.0.219:8001/api/",
+  baseURL: "http://192.168.0.219:32303/api/",
  //baseURL: process.env.REACT_APP_API_URL,
   //baseURL: "http://localhost:8001/api/",
 });
